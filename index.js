60187af01418c42ab14d41d7fccf4a8deb8874c0
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-11 13:47:50 +0600

    Add server site for world news left nav catagories.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express =require('express');
+const app= express();
+const port = process.env.PORT || 5000;
+
+const catagories=require('./Data/catagories.json')
+
+app.get('/', (req, res)=>{
+   res.send('world news running');
+});
+
+app.get('/catagories',(req, res)=>{
+    res.send(catagories)
+})
+app.listen(port, ()=>{
+   console.log(`world news running on port ${port}`)
+})
