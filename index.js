2df21af4cc405e4740be7a1e77f708114e47801d
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-09-12 00:14:07 +0600

    Loaded news, categories, search news id/category/all news  fields data.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 app.use(cors())
 
 const catagories=require('./Data/catagories.json')
+const news=require('./Data/news.json')
 
 app.get('/', (req, res)=>{
    res.send('world news running');
@@ -14,6 +15,28 @@ app.get('/', (req, res)=>{
 app.get('/catagories',(req, res)=>{
     res.send(catagories)
 })
+
+app.get('/news', (req, res)=>{
+   res.send(news)
+})
+
+app.get('/news/:id', (req, res)=>{
+   const id=req.params.id;
+   console.log(id);
+   const selectedNews=news.find(n=>n._id === id);
+   res.send(selectedNews);
+})
+
+app.get('/catagories/:id', (req, res)=>{
+   const id=req.params.id;
+   console.log(id);
+   const selectedCategory= news.filter(n=>n.category_id === id);
+   console.log(selectedCategory);
+   res.send(selectedCategory);
+})
+
+
+
 app.listen(port, ()=>{
    console.log(`world news running on port ${port}`)
 })
